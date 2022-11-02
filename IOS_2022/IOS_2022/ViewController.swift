31c8e388cb9e990d175c1e238233dc0206391975
Ardit <ardit@MacBook-Air-som-tillhor-Ardit.local>
2022-11-02 21:26:14 +0100

    Created Tabel View for MedTermin and view functions

--- IOS_2022/IOS_2022/ViewController.swift
+++ IOS_2022/IOS_2022/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var table: UITableView!
+    var models = [MedTermin]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
-
 
 }
 
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: IndexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: IndexPath)
+        cell.textLabel?.text = models[IndexPath.row].title
+        
+        return cell
+    }
+}
+
+struct MedTermin{
+    let title: String
+    let date: Date
+    let identifier: String
+}
